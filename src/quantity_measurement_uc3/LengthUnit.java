b288e34b0d42e5a7e0ef8a644ee0a59e7acba17c
Sahil Verma <braj91893@gmail.com>
2026-02-19 12:23:32 +0530

    [Sahil Verma] Add . Solved UC3 of Quantity Measurement App

---
+++ src/quantity_measurement_uc3/LengthUnit.java
@@ -0,0 +1,18 @@
+package quantity_measurement_uc3;
+
+
+public enum LengthUnit {
+
+    FEET(12.0),     
+    INCHES(1.0);    
+
+    private final double conversionFactor;
+
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+}
