e4dfd842e4892334c096a1e159d3bded32428d29
Sahil Verma <braj91893@gmail.com>
2026-03-10 14:48:57 +0530

    [Sahil Verma] Reafctor . Corrected length measurement and apply DRY principle

--- src/quantitymeasurement/QuantityMeasurementApp.java
+++ src/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,75 +3,7 @@ package com.apps.quantitymeasurement;
 import com.apps.quantitymeasurement.Length.LengthUnit;
 
 public class QuantityMeasurementApp {
-	
-	public static class Feet{
-		
-		private final double value ;
-		
-		public Feet(double value) {
-			this.value = value;	
-		}
-		
-		public double getValue() {
-			return value;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			
-			if(this == obj) return true;
-			if(obj==null || getClass()!=obj.getClass()) return false;
-			
-			Feet other = (Feet) obj;
-			
-			return Double.compare(this.value, other.value)==0;
-			
-		}
-		
-		@Override
-		public int hashCode() {
-			return Double.hashCode(this.value);
-		}
-		
-	}
-	
-	public static class Inches{
-		
-		private final double value;
-		
-		public Inches(double inches) {
-			
-			if(Double.isNaN(inches)) {
-				throw new IllegalArgumentException();
-			}
-			
-			this.value = inches;
-		}
-		
-		public double getValue() {
-			return value;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			
-			if(this==obj)return true;
-			
-			if(obj==null || getClass()!=obj.getClass()) return false;
-			
-			Inches other = (Inches) obj;
-			
-			return Double.compare(other.value, this.value)==0;
-			
-		}
-		
-		@Override
-		public int hashCode() {
-			return Double.hashCode(this.value);
-		}
-		
-	}
-	
+
 	
 	public static void demonstrateLengthEquality() {
 
